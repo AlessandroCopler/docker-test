9cc81e1c8b3a606a45b16f04bbc9c5c1a1226f43
AlessandroCopler <alessandro.copler@gmail.com>
2024-01-29 10:31:05 +0100

    aggiunto WSGIserver a flask per produzione

--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from gevent.pywsgi import WSGIServer
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,8 @@ def hello_world():
     return "Hello World"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port="5001")
+    # Debug/Development
+    # app.run(debug=True, host="0.0.0.0", port="5001")
+    # Production
+    http_server = WSGIServer(('', 5001), app)
+    http_server.serve_forever()
