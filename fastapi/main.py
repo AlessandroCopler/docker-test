324c3fd6ad9589995c0d2386d28995ead046ac31
AlessandroCopler <alessandro.copler@gmail.com>
2024-01-29 17:13:05 +0100

    Aggiunto esempio con netextender

--- fastapi/main.py
+++ fastapi/main.py
@@ -11,4 +11,5 @@ def read_root():
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
+    # http://127.0.0.1/items/5?q=somequery
     return {"item_id": item_id, "q": q}
